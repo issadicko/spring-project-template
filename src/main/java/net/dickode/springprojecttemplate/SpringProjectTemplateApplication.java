dd84058bad82b6b1bf5b2c15dc670953c404aae2
issadicko <issadicko78@gmail.com>
2023-05-07 19:01:20 +0000

    Initial commit

---
+++ src/main/java/net/dickode/springprojecttemplate/SpringProjectTemplateApplication.java
@@ -0,0 +1,13 @@
+package net.dickode.springprojecttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectTemplateApplication.class, args);
+    }
+
+}
---
+++ src/main/java/net/dickode/springprojecttemplate/dao/entity/User.java
@@ -0,0 +1,27 @@
+package net.dickode.springprojecttemplate.dao.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private String password;
+
+}
